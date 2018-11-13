4adf2bbc7058f934974be3ad7739143f16b62959
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2018-11-13 14:56:36 +0100

    inserito e salvati eta e km da prompt

--- js/main.js
+++ js/main.js
@@ -1,2 +1,8 @@
 //Chiedo tramite prompt l'eta dell'utente
 var eta = prompt("Salve, per acquistare il biglietto indichi la sua età");
+//console.log(eta);
+//Chiedo tramite prompt i km all'utente
+var km = prompt("Salve, per acquistare il biglietto indichi i chilometri da percorrere");
+//console.log(km);
+//salvo in variabile il costo chilometrico
+var costokm = 0.21;
