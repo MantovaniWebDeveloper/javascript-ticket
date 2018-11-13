2a798baa0ac64f38e8e0d66403987db0573ec1b1
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2018-11-13 15:09:18 +0100

    inserita la prima condizione completa con il calcolo sconto

--- js/main.js
+++ js/main.js
@@ -5,7 +5,16 @@ var eta = prompt("Salve, per acquistare il biglietto indichi la sua età");
 var km = prompt("Salve, per acquistare il biglietto indichi i chilometri da percorrere");
 //console.log(km);
 //salvo in variabile il costo chilometrico
-var costokm = 0.21;
+var costoKm = 0.21;
 //calcolo base per il costo del biglietto
-var totaleBiglietto = km * costokm;
-console.log(totaleBiglietto);
+var totaleBiglietto = km * costoKm;
+console.log("totale biglietto non scontato: " +totaleBiglietto);
+//inserisco la condizione
+if(eta < 18) {
+  //creo variabile che contiene il risultato dello sconto del 20%
+  var scontoMinorenni = totaleBiglietto * 20/100;
+  console.log("valore sconto: " + scontoMinorenni);
+  //creo una variabile che contiene il totale biglietto - lo sconto calcolato in precedenza
+  var totaleBigliettoScontato = totaleBiglietto - scontoMinorenni;
+  console.log("costo totale con sconto: " +totaleBigliettoScontato);
+}
